2244e23237ef0fe927d3e308e06835558027ec3d
WillianRaphael <98186784+WillianRaphael@users.noreply.github.com>
2023-05-11 21:53:52 -0300

    Update app.py

--- APIflask/app.py
+++ APIflask/app.py
@@ -2,24 +2,26 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-
 def calcular_fatorial(numero):
-    if numero < 0:
-        return "Número deve ser não-negativo."
-    elif numero == 0 or numero == 1:
+    if numero == 0:
         return 1
     else:
-        fatorial = 1
-        for i in range(1, numero + 1):
-            fatorial *= i
-        return fatorial
+        return numero * calcular_fatorial(numero - 1)
 
+def calcular_super_fatorial(numero):
+    super_fatorial = 1
+    for i in range(1, numero + 1):
+        super_fatorial *= calcular_fatorial(i)
+    return super_fatorial
 
-@app.route('/fatorial/<int:numero>', methods=['GET'])
-def obter_fatorial(numero):
-    resultado = calcular_fatorial(numero)
-    return jsonify({'numero': numero, 'fatorial': resultado})
-
+@app.route('/super_fatorial/<int:numero>')
+def rota_super_fatorial(numero):
+    resultado = calcular_super_fatorial(numero)
+    resposta = {
+        'numero': numero,
+        'super_fatorial': resultado
+    }
+    return jsonify(resposta)
 
 if __name__ == '__main__':
     app.run(debug=True)
